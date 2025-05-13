59b82efbdf1928a386359cbee5c22bb69d8485d3
Emanuele-Elias <s344489@studenti.polito.it>
2025-05-13 13:50:38 +0200

    ok

---
+++ agents/REINFORCE_baseline_value-net.py
@@ -0,0 +1,124 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+
+def discount_rewards(r, gamma):
+    discounted_r = torch.zeros_like(r)
+    running_add = 0
+    for t in reversed(range(0, r.size(-1))):
+        running_add = running_add * gamma + r[t]
+        discounted_r[t] = running_add
+    return discounted_r
+
+
+class Policy(torch.nn.Module):
+    def __init__(self, state_space, action_space):
+        super().__init__()
+        self.state_space = state_space
+        self.action_space = action_space
+        self.hidden = 64
+        self.tanh = torch.nn.Tanh()
+
+        """
+            Actor network
+        """
+        self.fc1_actor = torch.nn.Linear(state_space, self.hidden)
+        self.fc2_actor = torch.nn.Linear(self.hidden, self.hidden)
+        self.fc3_actor_mean = torch.nn.Linear(self.hidden, action_space)
+        
+        # Learned standard deviation for exploration at training time 
+        self.sigma_activation = F.softplus #to be sure self.sigma is pos
+        init_sigma = 0.5
+        self.sigma = torch.nn.Parameter(torch.zeros(self.action_space)+init_sigma) #parameters to be learnt: st. dev.
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if type(m) is torch.nn.Linear:
+                torch.nn.init.normal_(m.weight)
+                torch.nn.init.zeros_(m.bias)
+
+
+    def forward(self, x):
+        """
+            Actor
+        """
+        x_actor = self.tanh(self.fc1_actor(x))
+        x_actor = self.tanh(self.fc2_actor(x_actor))
+        action_mean = self.fc3_actor_mean(x_actor)
+
+        sigma = self.sigma_activation(self.sigma) #to be sure self.sigma is pos
+        normal_dist = Normal(action_mean, sigma)
+
+        
+        return normal_dist
+
+
+class Agent(object):
+    def __init__(self, policy, device='cpu'):
+        self.train_device = device
+        self.policy = policy.to(self.train_device)
+        self.optimizer = torch.optim.Adam(policy.parameters(), lr=1e-3)
+
+        self.gamma = 0.99
+        self.states = []
+        self.next_states = []
+        self.action_log_probs = []
+        self.rewards = []
+        self.done = []
+
+
+    def update_policy(self):
+        action_log_probs = torch.stack(self.action_log_probs, dim=0).to(self.train_device).squeeze(-1)
+        rewards = torch.stack(self.rewards, dim=0).to(self.train_device).squeeze(-1)
+
+        returns = discount_rewards(rewards, self.gamma)  
+
+        baseline  = returns.mean()      
+        advantage = returns - baseline  
+
+        T = returns.size(0)
+        discounts = (self.gamma ** torch.arange(T, 
+                          dtype=returns.dtype, 
+                          device=self.train_device))
+
+        policy_loss = -(discounts * action_log_probs * advantage).mean()
+
+        self.optimizer.zero_grad()
+        policy_loss.backward()
+        self.optimizer.step()
+
+        self.action_log_probs, self.rewards = [], []
+
+
+        return        
+
+
+    def get_action(self, state, evaluation=False):
+        """ state -> action (3-d), action_log_densities """
+        x = torch.from_numpy(state).float().to(self.train_device)
+
+        normal_dist = self.policy(x)
+
+        if evaluation:  # Return mean
+            return normal_dist.mean, None
+
+        else:   # Sample from the distribution
+            action = normal_dist.sample()
+
+            # Compute Log probability of the action [ log(p(a[0] AND a[1] AND a[2])) = log(p(a[0])*p(a[1])*p(a[2])) = log(p(a[0])) + log(p(a[1])) + log(p(a[2])) ]
+            action_log_prob = normal_dist.log_prob(action).sum()
+
+            return action, action_log_prob
+
+
+    def store_outcome(self, state, next_state, action_log_prob, reward, done):
+        self.states.append(torch.from_numpy(state).float())
+        self.next_states.append(torch.from_numpy(next_state).float())
+        self.action_log_probs.append(action_log_prob)
+        self.rewards.append(torch.Tensor([reward]))
+        self.done.append(done)
