4a7c1f815da0802259a527a57dc3643c5a9b7ae5
dalial89 <77731560+dalial89@users.noreply.github.com>
2025-05-25 11:11:51 +0200

    Create __init__.py

---
+++ SimOpt/__init__.py
@@ -0,0 +1,6 @@
+"""SimOpt – Adaptive Domain Randomization utilities and scripts.
+
+Importing this package registers nothing by default; run
+`python -m SimOpt.train` to start training or `python -m SimOpt.evaluate`
+for evaluation.
+"""
